61b49950b820a9b3a16ac86d8403568f415991a2
lixiafe <lx19890505@163.com>
2016-06-12 18:32:29 +0800

    修改数据库配置文件

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    dbUrl: 'mongodb://localhost/27017'
+    cookieSecret: '27ngkey',//用于Cookie加密与数据库无关
+    //dbUrl: 'mongodb://localhost/27017/27ng',
+    db: '27ng',
+    host: 'localhost',
+    port: 27017,
+    url: 'mongodb://localhost/27017/27ng'
 }
