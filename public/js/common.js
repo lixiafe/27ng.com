628907facdb2136d897f7077f3d3d50c7680942b
lixiafe <lx19890505@163.com>
2016-06-12 18:18:54 +0800

    视图,路由,数据库

--- public/js/common.js
+++ public/js/common.js
@@ -1,11 +1,3 @@
 window.onload = function(){
-    var oBtn = document.getElementById('user-login');
-    var cBtn = document.getElementById('close-login-btn');
-    var loginBox = document.getElementById('login-box');
-    cBtn.addEventListener('click', function(){
-        loginBox.style.display = 'none';
-    });
-    oBtn.addEventListener('click', function(){
-        loginBox.style.display = 'block';
-    });
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('个人中心页');
 });
 
-router.get('/add', function(req, res, next) {
-  res.send('respond with a add resource');
+router.get('/reg', function(req, res, next) {
+  res.render('user/reg', { title: '注册 - 爱去宁国' });
+});
+
+router.post('/reg', function(req, res, next) {
+    var user = req.body;
+    models.User.create(req.body, function(err, doc){
+        console.log(doc);
+        res.redirect('/users/login');
+    });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('index', { title: '登录 - 爱去宁国' });
+});
+
+router.get('/logout', function(req, res, next) {
+    res.render('index', { title: '退出 - 爱去宁国' });
 });
 
 module.exports = router;
