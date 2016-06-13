39bd2b149c5b9122ce01a0ff488856d64bc6c49d
lixiafe <lx19890505@163.com>
2016-06-13 18:23:52 +0800

    恢复index路由的修改

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '首页 - 爱去宁国' });
+  res.render('index', { title: '爱去宁国' });
 });
 
 router.get('/mingzhan', function(req, res, next) {
