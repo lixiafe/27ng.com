adfe99b830eab972a1f3561780f3540fc4971a3c
lixiafe <lx19890505@163.com>
2016-06-13 18:27:08 +0800

    增加session和render变量

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');//session中间件, req.session, 依赖cookieParser
+var MongoStore = require('connect-mongo')(session);
+var config = require('./config');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +24,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: '27ng',//加密的字符串
+  resave: true, //是否每次响应后重新保存session数据
+  saveUninitialized: true,//保存新创建但未初始化的session
+  store: new MongoStore({
+    url: config.dbUrl
+  })
+}));
+app.use(function(req, res, next){
+  //res.locals是模版渲染的对象, ejs里的对象传的的是
+  //app.render = function(name, options, callback){}
+  //merge(renderOptions, this.locals);
+  res.locals.user = req.session.user;
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
