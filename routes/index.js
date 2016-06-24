5f860f51db8a3efbc1e95194a776d978e6d0e1db
lixiafe <lx19890505@163.com>
2016-06-24 10:41:12 +0800

    首页路由完成, 分页路由未完成

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,42 @@ var router = express.Router();
 var models = require('../models');
 
 var viewMap = [
-  {'/': '首页'},
-  {'/mingzhan': '宁国名站', 'title': 爱去宁国},
-  {'/gouwu': '购物指南'},
-  {'/yule': '娱乐休闲'},
-  {'/meishi': '餐饮美食'},
-  {'/jiankang': '健康美容'},
-  {'/lvyou': '旅游出行'},
-  {'/shenghuo': '生活服务'},
-  {'/rencai': '人才教育'},
-  {'/fangchan': '房产'},
-  {'/zhengfu': '政府'},
-  {'/contact': '联系我们'}
+  {'url': '/', 'title': '首页 - 爱去宁国'},
+  {'url': '/mingzhan', 'title': '宁国名站 - 爱去宁国'},
+  {'url': '/gouwu', 'title': '购物指南 - 爱去宁国'},
+  {'url': '/yule', 'title': '娱乐休闲 - 爱去宁国'},
+  {'url': '/meishi', 'title': '餐饮美食 - 爱去宁国'},
+  {'url': '/jiankang', 'title': '健康美容 - 爱去宁国'},
+  {'url': '/lvyou', 'title': '旅游出行 - 爱去宁国'},
+  {'url': '/shenghuo', 'title': '生活服务 - 爱去宁国'},
+  {'url': '/rencai', 'title': '人才教育 - 爱去宁国'},
+  {'url': '/qiye', 'title': '企业 - 爱去宁国'},
+  {'url': '/fangchan', 'title': '房产 - 爱去宁国'},
+  {'url': '/zhengfu', 'title': '政府 - 爱去宁国'},
+  {'url': '/contact', 'title': '联系我们 - 爱去宁国'}
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.url);
-  models.Category.find({}, function(error, categories){
-    res.render('index', {title: '爱去宁国',categories: categories});
-  });
-});
+// router.get('/', function(req, res, next) {
+//   console.log(req.url);
+//   models.Category.find({}, function(error, categories){
+//     res.render('index', {title: '爱去宁国',categories: categories});
+//   });
+// });
 
-for(var i = 0; i < viewMap.length; i++){
-  router.get(viewMap[i], function(req, res, next) {
-      if(viewMap[i] == '/'){
-        models.Category.find({}, function(error, categories){
-      }
-
-      res.render('index', {title: '宁国名站 - 爱去宁国',categories: categories});
-    });
-  });
+for(let i = 0; i < viewMap.length; i++){
+	console.log(viewMap[i]["url"]);
+	router.get(viewMap[i]["url"], function(req, res, next) {
+	  if(viewMap[i]["url"] == '/'){
+	      models.Category.find({}, function(error, categories){
+		      res.render('index', {title: viewMap[i]["title"],categories: categories, reqUrl: viewMap[i]["url"]});
+	      });
+	  }else{
+		  models.Category.find({}, function(error, categories){
+			  res.render('index', {title: viewMap[i]["title"],categories: categories, reqUrl: viewMap[i]["url"]});
+		  });
+	  }
+	});
 }
 
 // router.get('/mingzhan', function(req, res, next) {
